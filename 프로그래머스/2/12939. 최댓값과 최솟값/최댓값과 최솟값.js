50c3ddabe0ea5db8c04e1cd57fc5ceb14423ecee
yeseul0809 <166012944+yeseul0809@users.noreply.github.com>
2024-05-11 23:38:00 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -5,10 +5,10 @@ function solution(s) {
   let answer = "";
 
   arr.sort((a, b) => a - b);
-  for (let i = 0; i < arr.length; i++) {
-    min = arr[0];
-    max = arr[arr.length - 1];
-  }
+
+  min = arr[0];
+  max = arr[arr.length - 1];
+
   answer = `${min} ${max}`;
   return answer;
 }
